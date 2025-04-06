24302e7237acd569cbb1144fe0c635a087d91fec
Ascens2He4vn <gageunderwood11@gmail.com>
2025-04-05 20:10:10 -0400

    come on billy! you can do it!

---
+++ src/main/java/net/ascens/condemnical/item/ChemItems.java
@@ -0,0 +1,19 @@
+package net.ascens.condemnical.item;
+
+import net.ascens.condemnical.Condemnical;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ChemItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Condemnical.MOD_ID);
+
+    //Vials
+    public static final DeferredItem<Item> EMPTY_VIAL = ITEMS.register("empty_vial", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> HYDROGEN_VIAL = ITEMS.register("hydrogen_gas_vial", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
