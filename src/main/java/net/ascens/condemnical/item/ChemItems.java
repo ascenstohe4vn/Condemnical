0c0c931a9e00dc53df1e927a8f1d87a265962d14
Ascens2He4vn <gageunderwood11@gmail.com>
2025-04-05 20:19:27 -0400

    billy's parents called the local police. they looked for months and months, but to no avail.

--- src/main/java/net/ascens/condemnical/item/ChemItems.java
+++ src/main/java/net/ascens/condemnical/item/ChemItems.java
@@ -13,7 +13,8 @@ public class ChemItems {
     public static final DeferredItem<Item> EMPTY_VIAL = ITEMS.register("empty_vial", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> HYDROGEN_VIAL = ITEMS.register("hydrogen_gas_vial", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> HELIUM_VIAL = ITEMS.register("helium_gas_vial", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> LITHIUM_VIAL = ITEMS.register("lithium_gas_vial", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> LITHIUM_VIAL = ITEMS.register("lithium_solid_vial", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> BERYLLIUM_VIAL = ITEMS.register("beryllium_solid_vial", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
